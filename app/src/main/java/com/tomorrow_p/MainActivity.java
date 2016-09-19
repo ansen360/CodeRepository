1fd01c5c704e47b0e4d8dc510977bb7186b3c573
tomorrow_p <tomorrow_p@163.com>
2016-09-19 21:06:17 +0800

    rm launcher icon

--- app/src/main/java/com/tomorrow_p/MainActivity.java
+++ app/src/main/java/com/tomorrow_p/MainActivity.java
@@ -9,20 +9,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.tomorrow_p.common.encrypt.EncryptTestActivity;
+import com.tomorrow_p.setupwizard.DefaultActivity;
+import com.tomorrow_p.view.FlowButton;
+import com.tomorrow_p.view.FlowLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FlowLayout mFlowLayout;
+    private MainActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_main);
-//        ImmersiveMode();
+        mFlowLayout = (FlowLayout) findViewById(R.id.flow_layout);
+
+        mFlowLayout.addView(new FlowButton(this, "EncryptTestActivity", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, EncryptTestActivity.class));
+            }
+        }));
+        mFlowLayout.addView(new FlowButton(this, "开机向导", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, DefaultActivity.class));
+            }
+        }));
+        mFlowLayout.addView(new FlowButton(this, "手势", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, com.tomorrow_p.gesture.MainActivity.class));
+            }
+        }));
 
 
-    }
-
-    public void encrypt(View v) {
-        startActivity(new Intent(this, EncryptTestActivity.class));
+        ImmersiveMode();
     }
 
     private void ImmersiveMode() {
@@ -62,18 +85,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {    // 沉浸式模式
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {    // 沉浸式模式
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+//            View decorView = getWindow().getDecorView();
+//            decorView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        }
+//    }
 }
---
+++ app/src/main/java/com/tomorrow_p/view/FlowButton.java
@@ -0,0 +1,43 @@
+package com.tomorrow_p.view;
+
+import android.content.Context;
+import android.widget.Button;
+
+/**
+ * Created by admin on 2016/9/19.
+ */
+public class FlowButton extends Button {
+
+    private static final int COLOR1 = 0xFF5698BB;
+    private static final int COLOR2 = 0xFF569874;
+    private static final int COLOR3 = 0xaa5698CD;
+    private static final int COLOR4 = 0x66CD9856;
+    private static final int PADDING = 10;
+
+    private static int[] COLORS = new int[]{COLOR1, COLOR2, COLOR3, COLOR4};
+    private static int COLOR_INDEX = 0;
+
+    private void nextColor() {
+        COLOR_INDEX = (COLOR_INDEX + 1) % COLORS.length;
+    }
+
+    public FlowButton(Context context, String buttonName, int backgroundRes, OnClickListener listener) {
+        super(context);
+        nextColor();
+        setText(buttonName);
+        setBackgroundColor(backgroundRes);
+        setOnClickListener(listener);
+        setTextSize(18.0f);
+        setPadding(PADDING, PADDING, PADDING, PADDING);
+    }
+
+    public FlowButton(Context context, String buttonName, OnClickListener listener) {
+        super(context);
+        nextColor();
+        setText(buttonName);
+        setBackgroundColor(COLORS[COLOR_INDEX % COLORS.length]);
+        setOnClickListener(listener);
+        setTextSize(18.0f);
+        setPadding(PADDING, PADDING, PADDING, PADDING);
+    }
+}
